f64d6f896fbce7c01f0827991bf4a133e7842b38
Felipe Silva <joaofelipems21@gmail.com>
2024-08-05 14:30:41 -0300

    UPDATES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./header/header.component";
 import { SeverStatusComponent } from "./dashboard/sever-status/sever-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SeverStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, SeverStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/dashboard/sever-status/sever-status.component.ts
+++ src/app/dashboard/sever-status/sever-status.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sever-status',
+  selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './sever-status.component.html',
