e7f1a98e612f41b243ab4c9c87adfa40b4d2d16e
Felipe Silva <joaofelipems21@gmail.com>
2024-08-20 14:33:11 -0300

    Adicionado NgOnInit

--- src/app/dashboard/sever-status/sever-status.component.ts
+++ src/app/dashboard/sever-status/sever-status.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './sever-status.component.html',
   styleUrl: './sever-status.component.css'
 })
-export class SeverStatusComponent {
-  currentStatus = 'online';
+export class SeverStatusComponent implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'unknown';
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ON INIT');
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
+  }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  }
 })
 export class ControlComponent {
   label = input<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Clicked');
+    console.log(this.el);
+  }
+
 }
